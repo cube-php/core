c1e8c5de3fb6eaf8a8d7548c714dbaf674ccef89
Damilola <damilolaofficial@gmail.com>
2023-05-30 17:19:37 +0100

    chore: [beta] package manager / installer

--- src/Helpers/Cli/Cli.php
+++ src/Helpers/Cli/Cli.php
@@ -24,6 +24,7 @@ use Cube\Commands\SessionMigrateCommand;
 use Cube\Commands\ViewClearCacheCommand;
 use Cube\Exceptions\CubeCliException;
 use Cube\Http\Env;
+use Cube\Misc\File;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Process\Process;
 
@@ -79,7 +80,87 @@ class Cli
             $application->add(new $class($this->app));
         });
 
+        $this->loadExtras($application);
         $application->run();
+    }
+
+    /**
+     * Load extra commands
+     *
+     * @param Application $application
+     * @return void
+     */
+    private function loadExtras(Application $application)
+    {
+        $dir = self::getExtrasDir();
+        $content = json_decode(file_get_contents($dir));
+
+        if (!$content) {
+            return;
+        }
+
+        every($content, function ($class) use ($application) {
+
+            if (!class_exists($class)) {
+                return self::removeExtraPackageCommand($class);
+            }
+
+            $application->add(
+                new $class($this->app)
+            );
+        });
+    }
+
+    /**
+     * Add extra package command
+     *
+     * @param array $commands
+     * @return void
+     */
+    public static function addExtraPackageCommand(array $commands)
+    {
+        $dir = self::getExtrasDir();
+        $content = json_decode(file_get_contents($dir));
+
+        if (!$content) {
+            $content = [];
+        }
+
+        every($commands, function ($class) use ($content, $dir) {
+            if (in_array($class, $content)) {
+                return;
+            }
+
+            $content[] = $class;
+            file_put_contents($dir, json_encode($content));
+        });
+    }
+
+    /**
+     * Remove command from extras
+     *
+     * @param string $class
+     * @return void
+     */
+    public static function removeExtraPackageCommand(string $class)
+    {
+        $dir = self::getExtrasDir();
+        $content = json_decode(file_get_contents($dir));
+
+        if (!$content) {
+            $content = [];
+        }
+
+        $values = array_values($content);
+
+        if (!in_array($class, $values)) {
+            return;
+        }
+
+        $index = array_search($class, $values);
+        unset($content[$index]);
+
+        file_put_contents($dir, json_encode($content));
     }
 
     /**
@@ -136,5 +217,29 @@ class Cli
         }
 
         return $content;
+    }
+
+    /**
+     * Get extra installed packages dir
+     *
+     * @return string
+     */
+    private static function getExtrasDir()
+    {
+        $app = App::getRunningInstance();
+        $cache_dir = $app->getPath(Directory::PATH_CACHE);
+        $packages_dir = $cache_dir . '/packages';
+
+        if (!is_dir($packages_dir)) {
+            mkdir($packages_dir);
+        }
+
+        $file_path = $packages_dir . '/packages.json';
+
+        if (!file_exists($file_path)) {
+            new File($file_path, true);
+        }
+
+        return $file_path;
     }
 }
