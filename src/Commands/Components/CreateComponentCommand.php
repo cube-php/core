3cb1568a678ec020164be5321b77e1a1935fb843
Damilola <damilolaofficial@gmail.com>
2024-08-15 16:41:42 +0100

    fix: removed unused import

--- src/Commands/Components/CreateComponentCommand.php
+++ src/Commands/Components/CreateComponentCommand.php
@@ -8,7 +8,6 @@ use Cube\Helpers\Cli\CliActions;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class CreateComponentCommand extends BaseCommand
